53d9c79992f7c05d3dc3ac53597f5f8fa0b84782
clipfail <rogroty@gmail.com>
2026-03-22 02:47:23 +0400

    Init

---
+++ Source/SurvivorsLike/SurvivorsLike.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SurvivorsLike.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SurvivorsLike, "SurvivorsLike" );
